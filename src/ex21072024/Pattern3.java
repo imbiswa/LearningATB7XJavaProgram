17a438f203b78ae44132bedf3754063111091d10
R N Biswajit mallick <rnbiswajitmallick48@gmail.com>
2024-07-21 19:27:26 +0530

    20-07-2024

--- src/ex21072024/Pattern3.java
+++ src/ex21072024/Pattern3.java
@@ -1,6 +1,7 @@
 package ex21072024;
 
-public class StarTriangle2 {
+public class Pattern3
+{
     public static void main(String[] args) {
 
 
@@ -18,6 +19,10 @@ public class StarTriangle2 {
                 System.out.print(" *");
             }
 
+            for(int j=1;j<i;j++)
+            {
+                System.out.print(" *");
+            }
             System.out.println();
         }
 
